a6ae58f12fd7a1ffc3ddf240b764d3638d40cb53
Poundcake <74396509+Poundcaked@users.noreply.github.com>
2025-02-24 17:07:35 -0800

    Working presence

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,15 +1,22 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.PresenceUpdate,
-    async execute(oldPresence, newPresence){     
-        const user = newPresence.member;
+    async execute(oldPresence, newPresence) {
+        if (!newPresence.member || !newPresence.guild) return; // Ensure valid presence update
 
-        if(newPresence.activities > 0){
-            if(user.presence.activities.find(activity => activity.type === 'PLAYING').applicationId == '700136079562375258'){
-                client.channels.cache.find((channel) => channel.name === 'general').send("P.U.!!!!! Someone in here just started playing Valorant! 🍗🍗🍗");
+        const channel = newPresence.guild.channels.cache.find(channel => channel.name === 'general');
+
+        const user = newPresence.member;
+        const playingActivity = newPresence.activities.find(activity => activity.type === ActivityType.Playing);
+
+        if (playingActivity) {     
+            if(playingActivity.applicationId === '700136079562375258'){
+                channel.send("P.U.!!!!! Someone in here just started playing Valorant! 🍗🍗🍗");
+            }
+            if(playingActivity.applicationId === '432980957394370572'){
+                channel.send("Fee fi fo fum.... I smell the blood of a Fortnite Player 👃👃👃");
             }
         }
     }
-        
 };
